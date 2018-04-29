5ea189746d4a6d416c7c36f409a9ceee269c9924
Gnxela <qawswedfrgtt@gmail.com>
2018-04-29 11:14:58 +0100

    Extracted DispatchPacket method and removed some semicolons.

--- GnPacket/netManager.go
+++ GnPacket/netManager.go
@@ -29,7 +29,7 @@ func New(queueLength int) NetManager {
 // The same handler can be added multible times, to the same or different IDs.
 // If a handler returns false, all subsiquent handlers will not fire.
 func (netManager *NetManager) AddHandler(id uint16, handler func(packet GnPacket) bool) {
-	netManager.handlers[id] = append(netManager.handlers[id], handler);
+	netManager.handlers[id] = append(netManager.handlers[id], handler)
 }
 
 // RemoveHandler removes a handler from netManager.
@@ -39,9 +39,23 @@ func (netManager *NetManager) RemoveHandler(id uint16, handler func(packet GnPac
 	if handlers, ok := netManager.handlers[id]; ok {
 		for i, handle := range handlers {
 			if fmt.Sprintf("%v", handle) == fmt.Sprintf("%v", handler) {
-				netManager.handlers[id] = append(netManager.handlers[id][:i], netManager.handlers[id][i + 1:]...);
+				netManager.handlers[id] = append(netManager.handlers[id][:i], netManager.handlers[id][i + 1:]...)
 			}
 		}
+	}
+}
+
+//DispatchPacket dispatches a packet to the relivant handlers.
+//If no handler has been assigned for a given pakcetID, the packet is pushed to NetManager.UnhandledQueue
+func (netManager *NetManager) DispatchPacket(packet GnPacket) {
+	if handlers, ok := netManager.handlers[packet.Id]; ok {
+		for _, handler := range handlers {
+			if !handler(packet) {
+				break
+			}
+		}
+	} else {
+		netManager.UnhandledQueue <- packet
 	}
 }
 
@@ -54,27 +68,19 @@ func (netManager *NetManager) ReadData(data *[]byte) {
 		if len(*data) < 4 {
 			break
 		}
-		packetLength := (*data)[:4];
+		packetLength := (*data)[:4]
 		length := binary.LittleEndian.Uint32(packetLength)
 		if (len(*data) >= int(length) && length > 0) {//If we have a completed packet
 
 			packetData := (*data)[:length]
 			*data = (*data)[length:]//Remove the data of the packet
 			
-			packetId := packetData[4:6];
+			packetId := packetData[4:6]
 			var id uint16 = uint16(packetId[0]) * 255 + uint16(packetId[1])
 			
 			packet := GnPacket{id, packetData[6:]}
-						
-			if handlers, ok := netManager.handlers[id]; ok {
-				for _, handler := range handlers {
-					if !handler(packet) {
-						break
-					}
-				}
-			} else {
-				netManager.UnhandledQueue <- packet;
-			}
+			
+			netManager.DispatchPacket(packet)
 		} else {
 			break
 		}
